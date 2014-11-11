795813b86a91b242660fcadecb52d96abbb20c07
Szabó Gyula <gyufi@niif.hu>
2014-11-11 16:56:03 +0100

    init

---
+++ composer.js
@@ -0,0 +1,9 @@
+{
+    "name": "niif/simplesamlphp-module-hexaa",
+    "description": "HEXAA attribute resolver authproc filter",
+    "type": "simplesamlphp-module",
+    "require": {
+        "simplesamlphp/composer-module-installer": "~1.0",
+        "niif/simplesamlphp-module-aa": "dev-master"
+    }
+}
